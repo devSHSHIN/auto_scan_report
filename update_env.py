e14297d1f4cfc2ab205452eafd67638090606d44
devSHSHIN <dev.shshin@gmail.com>
2024-09-11 15:02:20 +0900

    BUILD_ID 자동화 테스트

---
+++ update_env.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+current_folder = os.path.basename(os.getcwd())
+
+env_file_path = os.path.join(os.path.expandvars(os.getenv("SCRIPTS_PATH")), '.env')
+
+if not os.path.exists(env_file_path):
+    with open(env_file_path, 'w') as file:
+        file.write(f'BUILD_ID={current_folder}\n')
+else:
+    with open(env_file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(env_file_path, 'w') as file:
+        for line in lines:
+            if line.startswith('BUILD_ID='):
+                file.write(f'BUILD_ID={current_folder}\n')
+            else:
+                file.write(line)
+
+print(f'.env 파일이 업데이트되었습니다: NOW_DIR={current_folder}')
---
+++ use_env.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+build_id = os.getenv('BUILD_ID')
